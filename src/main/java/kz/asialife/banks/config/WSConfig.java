7acb95b3641bc6f517995d9cafdd98d98fffad51
NerD-JoKeR <kanjar_@mail.ru>
2018-07-02 23:51:25 +0600

    bcc integration v2

---
+++ src/main/java/kz/asialife/banks/config/WSConfig.java
@@ -0,0 +1,48 @@
+package kz.asialife.banks.config;
+
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WSConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/banks/*");
+    }
+
+    @Bean(name = "asialife")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("AsiaLifePort");
+        wsdl11Definition.setLocationUri("/banks");
+        wsdl11Definition.setTargetNamespace("http://asialife.kz/banks");
+        wsdl11Definition.setSchema(schema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema schema() {
+        return new SimpleXsdSchema(new ClassPathResource("schema/schema.xsd"));
+    }
+
+}
